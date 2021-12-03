382d1b38be348646ed769907fda4e9c687566327
mrmoon <abuamaramfreelance@gmail.com>
2021-12-03 19:52:27 +0200

    commit find component

---
+++ advancedTopicsInInternetProgramming/assignment/assignment1/20180358_assignment1/src/component/find/FindComponent.js
@@ -0,0 +1,11 @@
+export const FindComponent = ({link}) => {
+    return (
+        <div>
+            <a href={link}>
+                <button>
+                    Find out more
+                </button>
+            </a>
+        </div>
+    );
+};
