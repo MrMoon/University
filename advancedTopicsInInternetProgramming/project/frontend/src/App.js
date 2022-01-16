7cabbe672d26ff56e46149fc5873b5367742689c
mrmoon <abuamaramfreelance@gmail.com>
2022-01-16 17:41:14 +0200

    Add App UI

--- advancedTopicsInInternetProgramming/project/frontend/src/App.js
+++ advancedTopicsInInternetProgramming/project/frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import {Dashboard} from "./components/Dashboard";
+import {Homepage} from "./components/Homepage";
+import {Footer} from "./components/Footer";
+import {User} from "./components/User";
+import {PrivateRoute} from "./components/PrivateRoute";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/user">
+            <User />
+          </Route>
+          <PrivateRoute path="/dashboard">
+            <Dashboard/>
+          </PrivateRoute>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
   );
 }
-
-export default App;
